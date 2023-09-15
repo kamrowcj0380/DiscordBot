cc6f7a7219903c10ea2e4535cb4f682f2b58d1d2
kamrowcj0380 <connorkamrowski@gmail.com>
2023-09-15 13:06:17 -0500

    Added simple comments

--- main.py
+++ main.py
@@ -12,35 +12,42 @@ VERBOSE = 1
 
 def main():
     
-    #load the environment
+    #load the environment from file
     load_dotenv()
     TOKEN = os.getenv('DISCORD_TOKEN')
 
+    #set the intents of the bot to default
     intents = discord.Intents.default()
+    #allow the bot to see message content in 'intents'
     intents.message_content = True
+    #create the bot client
     client = discord.Client(intents=intents)
 
+    #print confirmation when bot connects
     @client.event
     async def on_ready():
         print(f'{client.user} has connected to Discord!')
-        
+    
+    #run when a message is sent
     @client.event
     async def on_message(msg):
         # return if the bot is the author
         if msg.author == client.user:
             return
         
+        #if verbose, show author and information
         if VERBOSE:
             print("Author:", msg.author)
             print("Content:", msg.content)
             print()
-            print("message itself:", msg)
 
+        #simple response testing
         if "test" in msg.content.lower():
             await msg.channel.send("Shawn was here")
         else:
-            await msg.channel.send("Shawn wasn't here")
+            await msg.channel.send("Where is Shawn...")
 
+    #run the client using the loaded token
     client.run(TOKEN)
 
 
