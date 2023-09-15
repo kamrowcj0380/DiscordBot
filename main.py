9f5dbf0f4ad93b16a48bbbfffed6fb011f61e2af
kamrowcj0380 <connorkamrowski@gmail.com>
2023-09-15 11:36:09 -0500

    created basics for main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+#Discord bot, purpose TBD
+#Authors: Andrew, Connor
+# bot.py
+import os
+import sys
+
+import discord
+
+if (len(sys.argv) != 1):
+    print("Insert Token as command line argument. Only one argument is allowed")
+
